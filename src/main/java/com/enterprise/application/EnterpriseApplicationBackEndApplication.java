2ed34fe0fab8d6593090a509a0de68305afdf2e3
jpabloallin <jpac.647@gmail.com>
2022-06-08 21:06:11 -0500

    Initial commit

---
+++ src/main/java/com/enterprise/application/EnterpriseApplicationBackEndApplication.java
@@ -0,0 +1,13 @@
+package com.enterprise.application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnterpriseApplicationBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EnterpriseApplicationBackEndApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/enterprise/application/EnterpriseApplicationBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package com.enterprise.application;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnterpriseApplicationBackEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
