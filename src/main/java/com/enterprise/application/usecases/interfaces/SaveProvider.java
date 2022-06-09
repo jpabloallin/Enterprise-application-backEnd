1d037ae30d4acdb75ed29410c07e376076f03d1c
jpabloallin <jpac.647@gmail.com>
2022-06-08 23:38:27 -0500

    Created SaveProvider interface that has ProviderDTO as a parameter.

---
+++ src/main/java/com/enterprise/application/usecases/interfaces/SaveProvider.java
@@ -0,0 +1,9 @@
+package com.enterprise.application.usecases.interfaces;
+
+import com.enterprise.application.model.ProviderDTO;
+import reactor.core.publisher.Mono;
+
+@FunctionalInterface
+public interface SaveProvider {
+    Mono<ProviderDTO> apply (ProviderDTO providerDTO);
+}
